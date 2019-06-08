d30c317c6af1fc84cc0839c7bff258b4bc771a96
Dmitry Figol <git@dmfigol.me>
2019-06-09 00:22:50 +0200

    Refactor create_repr

--- simple_smartsheet/utils.py
+++ simple_smartsheet/utils.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Sequence, Any
+from typing import Optional, Sequence, Any, List
 
 from marshmallow import EXCLUDE, RAISE
 
@@ -23,5 +23,10 @@ def is_debug() -> bool:
 def create_repr(obj: Any, attrs: Optional[Sequence[str]] = None):
     if attrs is None:
         attrs = obj.__dict__.keys()
-    attrs_repr = ", ".join(f"{attr}={getattr(obj, attr)!r}" for attr in attrs)
+    attrs_kv: List[str] = []
+    for attr in attrs:
+        attr_value = getattr(obj, attr)
+        if attr_value is not None:
+            attrs_kv.append(f"{attr}={attr_value!r}")
+    attrs_repr = ", ".join(attrs_kv)
     return f"{obj.__class__.__qualname__}({attrs_repr})"
